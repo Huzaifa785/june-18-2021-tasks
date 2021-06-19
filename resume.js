7cccd868a78d6b2b1f8e49f9d8861425a8f0e70b
Huzaifa Mohammed <huzaifa.coder785@gmail.com>
2021-06-19 10:17:11 -0700

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,22 @@
+resume = {
+    name:"Huzaifa Mohammed",
+    nationality: "India",
+    address: "Aurangabad, Maharashtra, India",
+    languages: ["English", "Hindi"],
+    skills: {"Front-End":[
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "Bootstrap"
+    ], 
+    "Back-End":[
+        "Python",
+        "JavaScript"
+    ],
+    "Web Frameworks":"Django"},
+    projects:["Security Camera: Made With Python",
+              "Library Management Software: Made With Python & Django"],
+    interests: ["Programming", "Football", "Connecting with new people & knowing about them"],
+    social_links: ["https://www.instagram.com/precious_huzaifa/",
+                   "https://www.linkedin.com/in/precioushuzaifa/"]
+}
